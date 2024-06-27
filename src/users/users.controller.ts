302e8e34ecba7f572c0d129dffdd3d7902d21f72
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-27 14:20:31 +0300

    feat(users): check if the user is not exist

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,7 +7,11 @@ import {
   Delete,
   Param,
   Query,
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
+
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -22,8 +26,12 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
   }
 
   @Get()
@@ -38,7 +46,7 @@ export class UsersController {
 
   @Patch('/:id')
   updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
-    console.log(body)
+    console.log(body);
     return this.usersService.update(parseInt(id), body);
   }
 }
