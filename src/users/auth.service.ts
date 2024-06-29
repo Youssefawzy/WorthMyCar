0118b8fd4f473bf67605f9397fb37c58b125e461
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-29 15:47:50 +0300

    feat(authentication): create the signup function

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,5 +1,10 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+import e from 'express';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -10,7 +15,15 @@ export class AuthService {
     if (users.length) {
       throw new BadRequestException('email in use');
     }
-  }
 
-  
+    const salt = randomBytes(8).toString('hex');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    const result = salt + '.' + hash.toString('hex');
+
+    const user = await this.usersService.create(email, result);
+
+    return user;
+  }
 }
