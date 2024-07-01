51365326f5918910f87b1c5f34db7c4d807de58f
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-07-01 21:15:10 +0300

    refactor : switch to test database  in test mode

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,17 +8,27 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/users.entity';
 import { Report } from './reports/report.entity';
-import { pipe } from 'rxjs';
+import { config, from, pipe } from 'rxjs';
 const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    TypeOrmModule.forRootAsync({
+      inject:[ConfigService],
+      useFactory: (config:ConfigService)=>{
+        return{
+          type:'sqlite',
+          database:config.get<string>('DB_NAME'),
+          synchronize:true,
+          entities:[User,Report]
+        }
+      }
+    }
+    ),
     UsersModule,
     ReportsModule,
   ],
