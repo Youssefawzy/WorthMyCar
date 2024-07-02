1d5e44948596c8a0711721f04a23cfa51716721f
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-07-02 18:06:54 +0300

    feat : parsing the query string

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -3,6 +3,8 @@ import {
   Controller,
   Post,
   Patch,
+  Get,
+  Query,
   Param,
   UseGuards,
 } from '@nestjs/common';
@@ -15,10 +17,16 @@ import { ReportDto } from './dtos/report.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from 'src/guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query);
+  }
 
   @Post()
   @UseGuards(AuthGuard)
